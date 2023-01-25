1ade2a17a0ea2922879c4f4b09161b35c8cab537
jamdaniels <cemayatac@proton.me>
2023-01-25 19:33:28 +0100

    changed index,  js, css

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("hi");
+
+function menuToggle() {
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
